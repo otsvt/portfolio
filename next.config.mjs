228d6439e6fe4995919c4818054487921b9b94d7
otsvt <heylalalay@mail.ru>
2024-06-18 19:06:20 +0300

    path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/portfolio",
 };
 
 export default nextConfig;
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,13 @@
-import { useRouter } from "next/router";
 import { useGSAPRoute } from "@/components/hooks/use-gsap-route";
 import "@/styles/globals.scss";
 import styles from "../styles/blocks/screen-loader.module.scss";
 
 export default function App({ Component, pageProps }) {
-  const { basePath } = useRouter();
   const { loaderRef } = useGSAPRoute();
 
   return (
     <>
-      <Component {...pageProps} basePath={basePath} />
+      <Component {...pageProps} />
       <div ref={loaderRef} className={styles.screenLoader}></div>
     </>
   );
