582878adba4c79ab446de38f80fe7bac3402705d
otsvt <heylalalay@mail.ru>
2024-06-19 10:51:52 +0300

    deploy fix

--- components/portfolio/portfolio.jsx
+++ components/portfolio/portfolio.jsx
@@ -17,11 +17,7 @@ export const Portfolio = () => {
 
   return (
     <div className={clsx(styles.body, "font-raleway")}>
-      <div
-        className={styles.wrapper}
-        style={{ backgroundImage: "url(./images/bg.jpg)" || "url(./portfolio/images/bg.jpg)" }}
-        ref={wrapperRef}
-      >
+      <div className={styles.wrapper} style={{ backgroundImage: "url(./images/bg.jpg)" }} ref={wrapperRef}>
         <Preloader isVisible={isLoading} />
         {!isLoading && (
           <div ref={contentRef} className={styles.content}>
