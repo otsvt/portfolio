6b975293b27ee9c20d6d6d5ecd97e523db84e3d8
otsvt <heylalalay@mail.ru>
2024-06-27 12:38:02 +0300

    render update

--- components/projects/weather/utils/render-icon.tsx
+++ components/projects/weather/utils/render-icon.tsx
@@ -4,10 +4,14 @@ import { OvercastIcon } from "../icons/weather/Overcast";
 import { RainyIcon } from "../icons/weather/Rainy";
 import { SunnyIcon } from "../icons/weather/Sunny";
 
-export const renderIcon = (type: string | undefined, className: string) => {
-  if (type === WeatherTypes.Sunny) return <SunnyIcon className={className} />;
-  if (type === WeatherTypes.Cloudy) return <CloudyIcon className={className} />;
-  if (type === WeatherTypes.Overcast) return <OvercastIcon className={className} />;
-  if (type === WeatherTypes.Rainy) return <RainyIcon className={className} />;
-  return <SunnyIcon className={className} />;
+export const renderIcon = (type: string, className: string) => {
+  const RenderIcon =
+    {
+      [WeatherTypes.Sunny]: SunnyIcon,
+      [WeatherTypes.Cloudy]: CloudyIcon,
+      [WeatherTypes.Overcast]: OvercastIcon,
+      [WeatherTypes.Rainy]: RainyIcon,
+    }[type] ?? SunnyIcon;
+
+  return <RenderIcon className={className} />;
 };
