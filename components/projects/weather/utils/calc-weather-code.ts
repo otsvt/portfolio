26174232df54306a422e954a8cc394debd52af0b
otsvt <heylalalay@mail.ru>
2024-06-27 12:24:27 +0300

    weather fix

--- components/projects/weather/utils/calc-weather-code.ts
+++ components/projects/weather/utils/calc-weather-code.ts
@@ -2,7 +2,7 @@ import { WeatherTypes } from "../types/enums";
 
 export const calcWeatherCode = (code: number) => {
   if (code === 0) return WeatherTypes.Sunny;
-  if (code === 1) return WeatherTypes.Cloudy;
+  if (code <= 2) return WeatherTypes.Cloudy;
   if (code < 55) return WeatherTypes.Overcast;
   if (code >= 55) return WeatherTypes.Rainy;
 
