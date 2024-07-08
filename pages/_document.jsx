1074a53c681500ad6ebf62fa59ccddf42132798e
otsvt <heylalalay@mail.ru>
2024-07-08 18:37:16 +0300

    fix favicon

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,11 +1,16 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import getConfig from "next/config";
+
+const { publicRuntimeConfig } = getConfig();
 
 export default function Document() {
+  const { basePath } = publicRuntimeConfig;
+
   return (
     <Html>
       <Head>
-        <link rel="icon" type="image/svg+xml" href="/images/favicons/favicon.svg" />
-        <link rel="icon" type="image/png" href="/images/favicons/favicon.png" />
+        <link rel="icon" type="image/svg+xml" href={`${basePath}/images/favicons/favicon.svg`} />
+        <link rel="icon" type="image/png" href={`${basePath}/images/favicons/favicon.png`} />
       </Head>
       <body>
         <Main />
