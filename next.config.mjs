63ff25bc3acca33d34a2fb2225ad745bc2dd1950
otsvt <heylalalay@mail.ru>
2024-08-02 19:04:50 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,11 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   basePath: isProd ? "/portfolio" : "",
   reactStrictMode: true,
+  output: "export",
   distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
   publicRuntimeConfig: {
     basePath: isProd ? "/portfolio" : "",
   },
