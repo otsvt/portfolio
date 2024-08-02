e5d63b6ae37fb86fa2fa7c54f41460ad3d500884
otsvt <heylalalay@mail.ru>
2024-08-02 18:46:48 +0300

    fix img

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   basePath: isProd ? "/portfolio" : "",
   reactStrictMode: true,
-  output: "export",
   distDir: "dist",
-  images: {
-    unoptimized: true,
-  },
   publicRuntimeConfig: {
     basePath: isProd ? "/portfolio" : "",
   },
